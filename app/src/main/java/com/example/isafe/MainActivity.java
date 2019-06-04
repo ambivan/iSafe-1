5113a0c219dcce0cb6afde2ac5636920003dff89
Tania Joseph <taniajoseph37@gmail.com>
2019-06-04 11:52:46 +0530

    Login Activity UI done.

--- app/src/main/java/com/example/isafe/MainActivity.java
+++ app/src/main/java/com/example/isafe/MainActivity.java
@@ -1,14 +1,19 @@
 package com.example.isafe;
 
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout linlay2;
+
+    Button login, signup;
+
 
     Handler handler = new Handler();
 
@@ -30,6 +35,17 @@ public class MainActivity extends AppCompatActivity {
         linlay2 = (LinearLayout) findViewById(R.id.linlay2);
 
         handler.postDelayed(runnable, 3000);
+
+        login = (Button) findViewById(R.id.login);
+        signup = (Button) findViewById(R.id.signup);
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
 
 
     }
