a2584b5e80e1fe2403e6430a674016ec342fdb90
Tania Joseph <taniajoseph37@gmail.com>
2019-06-04 11:26:05 +0530

    First Page UI done.

--- app/src/main/java/com/example/isafe/MainActivity.java
+++ app/src/main/java/com/example/isafe/MainActivity.java
@@ -1,15 +1,35 @@
 package com.example.isafe;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    LinearLayout linlay2;
+
+    Handler handler = new Handler();
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+
+            linlay2.setVisibility(View.VISIBLE);
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        linlay2 = (LinearLayout) findViewById(R.id.linlay2);
+
+        handler.postDelayed(runnable, 3000);
 
 
     }
